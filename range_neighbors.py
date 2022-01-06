bd1dd24efa84e1c54e4cc9adbcd7d67645419c5b
Mojtaba Mozaffar <mojtabamozaffar@gmail.com>
2022-01-06 13:25:01 -0600

    add test file

--- range_neighbors.py
+++ range_neighbors.py
@@ -1,5 +1,13 @@
 from typing import List
 
 
-def range_neighbors(A: List, B: list, ranges: List) -> List:
-    pass
+def range_neighbors(A: List, B: List, d1: int, d2: int, d3: int, d4: int, d5: int) -> List:
+
+    results = []
+
+    for point in A:
+        count_1, count2, count3, count4 = 0, 0, 0, 0
+
+
+if __name__ == "__main__":
+    range_neighbors([], [], 0, 0, 0, 0, 0)
